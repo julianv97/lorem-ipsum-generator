34cc69bd19a7813042b8fce552c25fc2328d8dc5
Julian Vicente <julian.vicente97s@gmail.com>
2021-08-04 11:37:18 -0300

    funcionamiento listo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import data from "./data";
 
 function App() {
+  const [text, setText] = useState([]);
+  const [count, setCount] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    setText(data.slice(0, amount));
+    /* CASOS:
+    count = 0
+    count < 0 
+    count > text.length
+    desarrollar un modal para estos casos
+    */
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className=" w-full flex flex-col items-center justify-center">
+      <h1 className="uppercase">tired of bored lorem ipsum?</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="uppercase">
+          generate
+        </button>
+      </form>
+      <article>
+        {text.map((paragraph, index) => {
+          return <p key={index}> {paragraph} </p>;
+        })}
+      </article>
+    </section>
   );
 }
 
